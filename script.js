860a3672875cc4fc55a7ebc10dbd4924dceee462
Dryden Griffin <208559256+drygriff@users.noreply.github.com>
2026-01-31 00:27:16 -0800

    fixed parse bugs and added headers, bold, underline, strikethrough, and escape char

--- script.js
+++ script.js
@@ -7,15 +7,8 @@ function getSetting(settingName) {
     return settingsForm.querySelector(`[name="${settingName}"]`).value
 }
 
-function updateOutput() { // Updates HTML directly so users can input HTML if they want
-    let outputText = textInput.textContent;
-
-
-    //italics
-    outputText = outputText.replace(/(?<!\\)\*(.+?)(?<!\\)\*/g, "<i>$1</i>");
-
-
-    outputText = `<div style="
+function wrapInOuterDiv(textToBeWrapped) {
+    return `<div style="
         width: 800px;
         background-color: ${getSetting("background-color")};
         color: ${getSetting("color")};
@@ -23,8 +16,38 @@ function updateOutput() { // Updates HTML directly so users can input HTML if th
         font-family: '${getSetting("font-family")}', 'Segoe UI', sans-serif;
         border-radius: ${getSetting("border-radius")}px;
         padding: ${getSetting("padding")}px;
-        ">${outputText}</div>`;
+    ">${textToBeWrapped}</div>`;
+}
 
+function fromMarkdown(inputMd) {
+    //headers
+    inputMd = inputMd.replace(/^(#{1,6}) (.+?)$/gm, `<h${"$1".length}>$2</h${"$1".length}>`);
+
+    // bold
+    inputMd = inputMd.replace(/(?<!\\)\*\*(.+?)(?<!\\)\*\*/gs, "<strong>$1</strong>");
+    // italics
+    inputMd = inputMd.replace(/(?<!\\)\*(.+?)(?<!\\)\*/gs, "<i>$1</i>");
+
+    // underline
+    inputMd = inputMd.replace(/(?<!\\)__(.+?)(?<!\\)__/gs, "<u>$1</u>");
+    // strikethrough
+    inputMd = inputMd.replace(/(?<!\\)~~(.+?)(?<!\\)~~/gs, "<strike>$1</strike>");
+
+
+    //remove escape key ex: "\*" -> "*"
+    inputMd = inputMd.replace(/\\(.)/g, "$1");
+
+    return inputMd;
+}
+
+function updateOutput() { // Updates HTML directly so users can input HTML if they want
+    let outputText = textInput.textContent;
+
+    outputText = fromMarkdown(outputText);
+    outputText = outputText.replace(/\r?\n/g, "<br>");
+
+
+    outputText = wrapInOuterDiv(outputText);
     previewCard.innerHTML = outputText;
 }
 
